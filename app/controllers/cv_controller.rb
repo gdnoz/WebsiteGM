67b313ebfac451a9c9b5094fcd68bc2825e798f8
gdnoz <gudnim@gmail.com>
2018-03-12 23:44:17 +0000

    Updated index page to open links in new tab

--- app/controllers/cv_controller.rb
+++ app/controllers/cv_controller.rb
@@ -1,6 +1,6 @@
 class CvController < ApplicationController
-  def index
-    pdf_filename = File.join(Rails.root, "app/assets/files/GM_CV.pdf")
-    send_file(pdf_filename, :filename => "GM_CV.pdf", :type => "application/pdf", :disposition => 'inline')
-  end
+    def index
+        pdf_filename = File.join(Rails.root, "app/assets/files/GM_CV.pdf")
+        send_file(pdf_filename, :filename => "GM_CV.pdf", :type => "application/pdf", :disposition => 'inline')
+    end
 end
