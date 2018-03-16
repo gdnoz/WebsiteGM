ece83e321cad953ad3c0c7eb315e9632102ba46c
gdnoz <gudnim@gmail.com>
2018-03-16 01:02:46 +0000

    Beautified it and added some lorem ipsum to front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'start_page#index'
+
   get 'cv', to: 'cv#index'
+
   get 'todos', to: 'todos#index'
   post 'todos', to: 'todos#create'
   put 'todos/:id', to: 'todos#update'
